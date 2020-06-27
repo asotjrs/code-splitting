1d326859d1f32b9f29e043b06785ce912c905642
unknown <asotjrs@gmail.com>
2020-06-27 23:45:58 +0200

    adding the Async object  to code splitting

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Page1 from "./components/page1";
-import Page2 from "./components/page2";
-import Page3 from "./components/page3";
+import AsyncComponent from "./components/AsyncComponent";
 
 class App extends Component{
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state={
       route:'page1'
-
     }
 
   }
   onRouteChange=(route)=>{
-    this.setState({route:route})
+    this.setState({route:route});
+
   };
   render()
   {
-    if (this.state.route==='page1')
-      return <Page1 onRouteChange={this.onRouteChange}/>
-    else if(this.state.route==='page2')
-      return <Page2 onRouteChange={this.onRouteChange} />
+      if (this.state.route==='page1')
+      { const AsyncPage1= AsyncComponent(()=>import('./components/page1'));
+        return <AsyncPage1 onRouteChange={this.onRouteChange} />;
+      }     else if(this.state.route==='page2')
+      { const AsyncPage2= AsyncComponent(()=>import('./components/page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+      }
     else
-      return <Page3 onRouteChange={this.onRouteChange}/>
-
-
-
+    { const AsyncPage3= AsyncComponent(()=>import('./components/page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;}
 
   }
 
